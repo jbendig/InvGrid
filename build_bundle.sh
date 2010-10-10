e993a3b36f59d4db4547c8a1222472cf861d17dd
James Bendig <jbendig@starbytesoftware.com>
2010-10-10 12:47:15 -0400

    Added application icon donated by Lachy17689 on minecraftforum.net.
    Thanks!

--- build_bundle.sh
+++ build_bundle.sh
@@ -1,5 +1,7 @@
 rm -rf InvGrid.app
 mkdir -p InvGrid.app/Contents/MacOS
+mkdir -p InvGrid.app/Contents/Resources
 cp Info.plist InvGrid.app/Contents
+cp invgrid.icns InvGrid.app/Contents/Resources/
 mv InvGrid InvGrid.app/Contents/MacOS
 macdeployqt InvGrid.app -dmg
